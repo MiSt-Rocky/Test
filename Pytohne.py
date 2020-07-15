450ba7e7bdec127272c582f99096ef40bf2f9d8d
MiSt-Rocky <67986306+MiSt-Rocky@users.noreply.github.com>
2020-07-15 12:11:29 -0500

    Add files via upload

---
+++ Pytohne.py
@@ -0,0 +1,18 @@
+print('''|\/|
+|\-/|\-/|\-/|''')
+for y in "antidisestablishmentarianism":
+  print(y)
+  
+print('''        ^
+        ()
+         v''')
+fruits = ["apple", "banana", "cherry", "pear", "windows", "linux", "fat"]
+for x in fruits:
+  print(x)
+  if x == "pear":
+    print('lololollolol')
+for x in range(100):
+  print(x)
+else:
+  print("Finally finished!")
+print('done.')
